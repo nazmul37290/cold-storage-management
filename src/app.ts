2c2124e6eacd76156ea5e4e60c277fc59d74462f
nazmul37290 <nazmulnjm@gmail.com>
2025-12-04 01:01:08 +0600

    updated

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import router from './app/routes';
 const app: Application = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin:true,credentials:true}));
 
 app.use('/api', router);
 app.get('/', (req: Request, res: Response) => {
